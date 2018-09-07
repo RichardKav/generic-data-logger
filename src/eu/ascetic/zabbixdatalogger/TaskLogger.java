a99cdc43d397a73cc9b38bc9d935ffae92992b88
Richard Kavanagh <r_kav_0000@hotmail.com>
2018-09-07 10:45:38 +0100

    Adding an average jobs completed
    This average is calculated manually compare the last record to the
    current one.
    This aims to target fast running jobs.
    It also ensures the aveage is windowed so will update more readily over
    time.

--- src/eu/ascetic/zabbixdatalogger/TaskLogger.java
+++ src/eu/ascetic/zabbixdatalogger/TaskLogger.java
@@ -47,6 +47,7 @@ public class TaskLogger extends GenericLogger<CompssImplementation> {
         store.append("Average Time");
         store.append("Min Time");
         store.append("Max Time");
+        store.append("Spot Average Time");
     }
 
     @Override
@@ -59,6 +60,30 @@ public class TaskLogger extends GenericLogger<CompssImplementation> {
         store.append(item.getAverageTime());
         store.append(item.getMinTime());
         store.append(item.getMaxTime());
+        store.append(getSpotRunningTime(item, cal.getTimeInMillis()));
+    }
+    
+    /**
+     * This gets the spot average runtime of an application - compss reports the
+     * average runtime for all time, so this does the time since the last record was passed.
+     * @param item The current compss implementation record undergoing writing to disk
+     * @param time The current time, in milliseconds
+     * @return The rate at which jobs have been processed
+     */
+    private double getSpotRunningTime(CompssImplementation item, long time) {
+        double answer = 0;
+        long currentTime = time / 1000;       
+        Long lastTimeValue = previousTime.get(item.getName());
+        CompssImplementation previousItem = previous.get(item.getName());
+        if (lastTimeValue != null && previousItem != null) {
+            double changeInExecutionCount = item.getExecutionCount() - previousItem.getExecutionCount();
+            long changeInTime = currentTime - lastTimeValue;
+            answer = changeInExecutionCount / changeInTime;
+        }
+        //make a record of the previous item seen
+        previousTime.put(item.getName(), currentTime);
+        previous.put(item.getName(), item);
+        return answer;
     }
     
 }
